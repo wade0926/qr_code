484246764e6ea490d464c7998c456f1e4c1e5922
wade0926 <posipsy@gmail.com>
2015-08-04 18:33:35 +0800

    b

--- readme.php
+++ readme.php
@@ -1,7 +1,14 @@
-1. ¹Ï¤ùÂà´«¦¨php echo Åã¥Ü¥X¨Ó¡G
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>QR Code</title>
+</head>
+1. åœ–ç‰‡è½‰æ›æˆphp echo é¡¯ç¤ºå‡ºä¾†ï¼š
    http://davidwalsh.name/data-uri-php
    ----------
    $image = 'http://s01.calm9.com/qrcode/2015-08/NTRX3PYRLN.png';
    $imageData = base64_encode(file_get_contents($image));
    $src = 'data: '.mime_content_type($image).';base64,'.$imageData;
    echo '<img src="',$src,'">';
+</html>
